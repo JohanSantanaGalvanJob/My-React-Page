156c0121b59010a51d11fe52a5cbe50297514e26
Johan <JohanSantanaGalvan.Job@gmail.com>
2022-05-19 12:16:00 +0100

    trying to add a video in a-frame

--- src/components/AFrame/AFrame.js
+++ src/components/AFrame/AFrame.js
@@ -1,15 +1,25 @@
+import { render } from '@testing-library/react';
+import 'aframe';
 import { Entity, Scene } from 'aframe-react'
 import React from 'react'
 import video1 from '../../assets/video/Route-1/Route-1-Stop-3.mp4'
 
- const AFrameProject = () => {
-   <Scene>
-       <a-assets>
-          <video id='skyTexture' src={video1} alt='skyTexture'/>
-       </a-assets>
-       <Entity primitive="a-plane" rotation="-90 0 0" height="1000"
-       width="1000" material={{color:"#215763",opacity:0.70}}/>
+ class AFrameProject extends React.Component {
+   render() {
+     return(
+    <Scene>
+      <a-assets>
+        <video id="Route-1-Stop-3" src={video1}></video>
+      </a-assets>
+       <Entity geometry={{primitive: 'box'}} material={{color: 'red'}} position={{x: 0, y: 0, z: -5}}/>
+        <Entity light={{type: 'point'}}/>
+        <Entity gltf-model={{src: 'virtualcity.gltf'}}/>
+        <Entity text={{value: 'Hello, WebVR!'}}/>
+        <a-videosphere src="Route-1-Stop-3" rotation="0 -90 0"></a-videosphere>
    </Scene>
+     );
+   }
+   
  }
 
 
