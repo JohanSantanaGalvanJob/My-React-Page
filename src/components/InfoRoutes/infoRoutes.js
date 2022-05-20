9d7f66aab7d0a3120e4e2b7c72067e80f1c6f13e
JordanJTY <Darkjordanrayo@gmail.com>
2022-05-20 19:27:55 +0100

    AFrame low advance

--- src/components/InfoRoutes/infoRoutes.js
+++ src/components/InfoRoutes/infoRoutes.js
@@ -10,7 +10,7 @@ export const infoRoutes = [
         idStop: 1,
         name: "Camino a la iglesia",
         description: "",
-        video: "",
+        video: "Route-1-Stop-1.mp4",
         location: [27.9086217,-15.3886205]
       }, {
         idStop: 2,
@@ -22,7 +22,7 @@ export const infoRoutes = [
         idStop: 3,
         name: "Plaza del Rosario",
         description: "",
-        video: "",
+        video: "Route-1-Stop-3.mp4",
         location: [27.9066994,-15.4490244]
       }, {
         idStop: 4,
@@ -34,7 +34,7 @@ export const infoRoutes = [
         idStop: 5,
         name: "Casa de la cámara episcopal",
         description: "",
-        video: "",
+        video: "Route-1-Stop-5.mp4",
         location: ""
       },
     ]
@@ -49,31 +49,55 @@ export const infoRoutes = [
         idStop: 1,
         name: "Entrada a la guagua",
         description: "",
-        video: "",
+        video: "Route-2-Stop-1.mp4",
         location: [27.9086217,-15.3886205]
       }, {
         idStop: 2,
-        name: "Iglesia",
+        name: "Avenida Marítima",
         description: "",
-        video: "",
+        video: "Route-2-Stop-2.mp4",
         location: [27.9560056,-15.4518982]
       }, {
         idStop: 3,
-        name: "Plaza del Rosario",
+        name: "Santa Catalina",
         description: "",
-        video: "",
+        video: "Route-2-Stop-3.mp4",
         location: [27.9560056,-15.4518982]
       }, {
         idStop: 4,
         name: "Casa de los Camellos",
         description: "",
-        video: "",
+        video: "Route-2-Stop-4.mp4",
         location: [27.9560056,-15.4518982]
       }, {
         idStop: 5,
-        name: "Casa de la cámara episcopal",
+        name: "Mercado Puerto",
         description: "",
-        video: "",
+        video: "Route-2-Stop-5.mp4",
+        location: [27.9560056,-15.4518982]
+      }, {
+        idStop: 6,
+        name: "Iglesia roja y el Corte Inglés",
+        description: "",
+        video: "Route-2-Stop-6.mp4",
+        location: [27.9560056,-15.4518982]
+      }, {
+        idStop: 7,
+        name: "Teatro y Mercado",
+        description: "",
+        video: "Route-2-Stop-7.mp4",
+        location: [27.9560056,-15.4518982]
+      }, {
+        idStop: 8,
+        name: "Biblioteca de San Telmo",
+        description: "",
+        video: "Route-2-Stop-8.mp4",
+        location: [27.9560056,-15.4518982]
+      }, {
+        idStop: 9,
+        name: "Mercado",
+        description: "",
+        video: "Route-2-Stop-9.mp4",
         location: [27.9560056,-15.4518982]
       },
     ]
@@ -131,53 +155,23 @@ export const infoRoutes = [
     stops: [
       {
         idStop: 1,
-        name: "Rotonda del árbol del responso",
+        name: "Benito Pérez Galdós",
         description: "",
-        video: "",
+        video: "Route-4-Stop-1.mp4",
         location: [28.0961117,-15.4161696]
       }, {
         idStop: 2,
-        name: "Natalia Sosa Ayala",
+        name: "Domingo J. Navarro",
         description: "",
-        video: "",
+        video: "Route-4-Stop-2.mp4",
         location: [27.9560056,-15.4518982]
       }, {
         idStop: 3,
-        name: "Domingo J Navarro",
+        name: "Natalia Sosa Ayala",
         description: "",
-        video: "",
+        video: "Route-4-Stop-3.mp4",
         location: [27.9560056,-15.4518982]
-      }, {
-        idStop: 4,
-        name: "Agatha Christie",
-        description: "",
-        video: "",
-        location: [27.9560056,-15.4518982]
-      }, {
-        idStop: 5,
-        name: "Tomás Morales",
-        description: "",
-        video: "",
-        location: [27.9560056,-15.4518982]
-      }, {
-        idStop: 6,
-        name: "Josefina de la Torre",
-        description: "",
-        video: "",
-        location: [27.9560056,-15.4518982]
-      }, {
-        idStop: 7,
-        name: "Carmen Laforet",
-        description: "",
-        video: "",
-        location: [27.9560056,-15.4518982]
-      }, {
-        idStop: 8,
-        name: "Benito Pérez Galdós",
-        description: "",
-        video: "",
-        location: [27.9560056,-15.4518982]
-      },
+      }, 
     ]
   }, {
     id: 5,
@@ -190,19 +184,19 @@ export const infoRoutes = [
         idStop: 1,
         name: "Iglesia de Santa Brígida",
         description: "",
-        video: "",
+        video: "Route-5-Stop-1.mp4",
         location: [28.0653687,-15.4627028]
       }, {
         idStop: 2,
         name: "Bosque de Laurisilva",
         description: "",
-        video: "",
+        video: "Route-5-Stop-2.mp4",
         location: [27.9560056,-15.4518982]
       }, {
         idStop: 3,
         name: "Fuente de los Sabios",
         description: "",
-        video: "",
+        video: "Route-5-Stop-3.mp4",
         location: [28.0653687,-15.4627028]
       },
     ]
@@ -244,6 +238,123 @@ export const infoRoutes = [
         video: "",
         location: [27.9560056,-15.4518982]
       },
+    ]
+  }, {
+    id: 7,
+    color: "orange",
+    name: "Barranco de Azuaje",
+    description: "Ruta de Azuaje realizada por el alumnado de 2ºDAM-T del IES El Rincón",
+    music: "",
+    stops: [
+      {
+        idStop: 1,
+        name: "Oficina de Turismo",
+        description: "",
+        video: "Route-7-Stop-1.mp4",
+        location: [27.9560056,-15.4518982]
+      }, {
+        idStop: 2,
+        name: "Haciendo cochafisco",
+        description: "",
+        video: "Route-7-Stop-2.mp4",
+        location: [27.9560056,-15.4518982]
+      }, {
+        idStop: 3,
+        name: "La Sala de honor",
+        description: "",
+        video: "Route-7-Stop-3.mp4",
+        location: [27.9560056,-15.4518982]
+      }, {
+        idStop: 4,
+        name: "Empezando el camino",
+        description: "",
+        video: "Route-7-Stop-4.mp4",
+        location: [27.9560056,-15.4518982]
+      }, {
+        idStop: 5,
+        name: "Mirador del Barranco de Azuaje",
+        description: "",
+        video: "Route-7-Stop-5.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 6,
+        name: "Bajando al Barranco",
+        description: "",
+        video: "Route-7-Stop-6.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 7,
+        name: "El Camino empedrado del Barranco",
+        description: "",
+        video: "Route-7-Stop-7.mp4",
+        location: [27.9560056,-15.4518982]
+      }, {
+        idStop: 8,
+        name: "Caminando junto al Arroyo",
+        description: "",
+        video: "Route-7-Stop-8.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 9,
+        name: "Una Poceta llena de Mosquitos",
+        description: "",
+        video: "Route-7-Stop-9.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 10,
+        name: "Voy cruzando el río",
+        description: "",
+        video: "Route-7-Stop-10.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 11,
+        name: "Más gente cruzando el río",
+        description: "",
+        video: "Route-7-Stop-11.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 12,
+        name: "Cruzando el río otra vez",
+        description: "",
+        video: "Route-7-Stop-12.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 13,
+        name: "En medio del barranco",
+        description: "",
+        video: "Route-7-Stop-13.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 14,
+        name: "El balneario",
+        description: "",
+        video: "Route-7-Stop-14.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 15,
+        name: "El agua no deja pasar",
+        description: "",
+        video: "Route-7-Stop-15.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 16,
+        name: "Un palmeral impresionante",
+        description: "",
+        video: "Route-7-Stop-16.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 17,
+        name: "El Cañaveral",
+        description: "",
+        video: "Route-7-Stop-17.mp4",
+        location: [27.9560056,-15.4518982]
+      },  {
+        idStop: 18,
+        name: "Llegando a nuestro destino",
+        description: "",
+        video: "Route-7-Stop-18.mp4",
+        location: [27.9560056,-15.4518982]
+      }
     ]
   },
 ];
--- src/pages/AFrame/AFrame.js
+++ src/pages/AFrame/AFrame.js
@@ -1,36 +1,41 @@
-import { render } from '@testing-library/react';
 import 'aframe';
-import { Entity, Scene } from 'aframe-react'
-import React from 'react'
-import video1 from '../../assets/video/Route-5/Route-5-Stop-3.mp4'
-import AFrameHud from '../../components/AFrameHud/AFrameHud';
+import './AFrame.css';
+import { setVideoActive } from '../../components/AFrameScript/Video360Click.js';
+import { useEffect } from 'react';
+import MenuItem from '../../components/AFrameScript/MenuItem';
+import { useParams } from 'react-router-dom';
+import { infoRoutes } from '../../components/InfoRoutes/infoRoutes';
 
-class AFrameProject extends React.Component {
-  render() {
-    return (
-      <Scene>
-        <AFrameHud />
+export default function AFrameProject() {
+  const { route, stop } = useParams();
+
+  useEffect(() => {
+    setVideoActive();
+  }, []);
+
+  return (
+    <div className="my-container">
+
+      <a-scene>
         <a-assets>
-          <video id="Route-1-Stop-3" loop={true} src={video1} autoPlay="true" />
-          <img id="" src=''/>
+          <video id="vid" loop={true} src={`src/assets/video/Route-${route}/Route-${route}-Stop-${stop}.mp4`} autoPlay={true} > </video>
         </a-assets>
 
         <a-entity camera="" position="0 1.6 0" look-controls="" >
-          <a-entity cursor="fuse:true;fuseTimeout:2000" geometry="primitive:ring;radiusInner:0.01;radiusOuter:0.02" position="0 0 -1.8" material="shader:flat;color:#008000" animation__mouseenter="from:1 1 1;dir:reverse;dur:2000;property:scale;startEvents:mouseenter;to:4 4 4"
+          <a-entity cursor="fuse:true;fuseTimeout:2000" geometry="primitive:ring;radiusInner:0.01;radiusOuter:0.02" position="0 0 -1.8" material="shader:flat;color:#FFFFF" animation__mouseenter="from:1 1 1;dir:reverse;dur:2000;property:scale;startEvents:mouseenter;to:4 4 4"
             raycaster="objects: .clickable">
           </a-entity>
         </a-entity>
+        {
+          infoRoutes[route - 1].stops.map((s, index) =>
+            <MenuItem key={index} x=".8" y="1.6" pos={index - 1} route={route} stop={index} textToShow={s.name} available={s.video !== ""} />
+          )
+        }
 
-        <a-videosphere src="#Route-1-Stop-3" rotation="0 0 0" />
 
-        <a-plane position="2.5 3 -1.3" rotation="160 110 0" height="0.5" width="0.5"  />
 
-      </Scene>
-    );
-  }
-
+        <a-videosphere src="#vid" ></a-videosphere>
+      </a-scene>
+    </div>
+  );
 }
-
-
-
-export default AFrameProject
